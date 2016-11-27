4cf326f6ee5f557d5dd0972ef82655abb6a61cd2
硕杨 <448793925@qq.com>
2016-11-27 19:44:36 +0800

    Revert "no message"
    This reverts commit 7c4b3a5bb4c12c9c086857aef6bf18dbabbc3e9b.

---
+++ SourceTreeTest/SourceTreeTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SourceTreeTest
+//
+//  Created by 硕杨 on 16/11/27.
+//  Copyright © 2016年 C05. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
