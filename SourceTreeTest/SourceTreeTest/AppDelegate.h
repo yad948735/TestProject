b7292c84f6b100333398c77eed51c996e45dd9f1
硕杨 <448793925@qq.com>
2016-11-27 17:14:51 +0800

    项目创建

---
+++ SourceTreeTest/SourceTreeTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SourceTreeTest
+//
+//  Created by 硕杨 on 16/11/27.
+//  Copyright © 2016年 C05. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
